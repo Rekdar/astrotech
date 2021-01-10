a3e52f04738db40b8535d8f6cdd1b99932a8c7eb
rk <rk@rk.com>
2021-01-10 13:47:38 +0100

    Kolejne wyjątki dodane do pliku

--- Exceptions.py
+++ Exceptions.py
@@ -1,15 +1,45 @@
-user_temp = input('Wprowadź temperature w Kelwinach: ')
+# x = 'hello'
+# assert x == 'hello'
+# assert x == 'abc'
+# **************************************************************************
+# user_temp = input('Wprowadź temperature w Kelwinach: ')
+#
+# class ValueToSmall(Exception):
+#     pass
+#
+# def check_temp(u_temp):
+#     try:
+#         temp = float(u_temp)
+#         if temp < 0:
+#             raise ValueToSmall('Temperatura jest mniejsza od 0, podaj większą od 0')
+#             pass
+#     except ValueError:
+#         print('Musisz podać wartość liczbową')
+#         temp = None
+#     return temp
+#
+# print(check_temp(user_temp))
 
+# **************************************************************************
 
-def check_temp(u_temp):
+# user_age = input('Podaj swój wiek: ')
+#
+# if int(user_age) < 18:
+#     raise PermissionError('Adults only')
+
+# **************************************************************************
+
+user_value = input('Wprowadź stopie kąta figury geometrycznej: ')
+
+class CotangentError(Exception):
+    pass
+
+def geometria(u_val):
     try:
-        temp = int(u_temp)
+        degree = float(user_value)
+        if degree > 180:
+            raise CotangentError('Wartość musi być mniejsza niż 180 stopni')
     except ValueError:
-        print('Muszisz podać wartość liczbową')
-        temp = None
-    return temp
-
-
-#check_temp(user_temp)
-
-print(check_temp(user_temp))
+        print('Kąt musi być wartością numeryczną')
+    return degree
+print(geometria(user_value))
